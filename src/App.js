7db6dd7dfb9a984d4fc6b750f7b9539d287f5039
Gnanashekar <131336822+gnanashekar-s@users.noreply.github.com>
2024-05-11 12:13:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import HelloWorld from "./components/HelloWorld";
+
+function App() {
+  return <div>
+    <HelloWorld>
+
+    </HelloWorld>
+  </div>
+}
+   
+
+export default App;
---
+++ src/components/HelloWorld.js
@@ -0,0 +1,11 @@
+import Button from "./Button";
+
+function HelloWorld() {
+    return (<div>
+         <h1>Hello World!</h1>
+    <Button/>
+    </div>
+   )
+}
+
+export default HelloWorld;
